b70045b9b6756348f779dd4a08953034a05a8428
Scherrer01 <diogo.scherrer@gmail.com>
2026-02-23 16:33:37 -0300

    Contador

---
+++ stateful_app/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: SorteadorPage(),
+  ));
+}
+
+class SorteadorPage extends StatefulWidget {
+  @override
+  _SorteadorPageState createState() => _SorteadorPageState();
+}
+
+class _SorteadorPageState extends State<SorteadorPage> {
+  // Variável para armazenar o número sorteado
+  int numero = 0;
+  
+  // Função para sortear número
+  void sortear() {
+    setState(() {
+      numero = Random().nextInt(10) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sorteador de Números"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Número sorteado:"),
+            Text(
+              numero == 0 ? "?" : numero.toString(),
+              style: TextStyle(fontSize: 50),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: sortear,
+              child: Text("SORTEAR"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
